6498255700e1c860f37e7e3716e88edf929730d0
alanjames00 <alanjames6282@gmail.com>
2023-10-28 21:26:55 +0530

    add childprocess and update to modules

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 // npx youtube-exec audio --url https://www.youtube.com/watch\?v\=bx3Vy0OmE8k --folder "downloads" --quality "best"
 
 // Uses project-only version of youtube-exec npm package
+
+// On Windows Only...
+import { spawn } from 'node:child_process';
+
+const bat = spawn('cmd.exe', ['/c', 'my.bat']);
+
+bat.stdout.on('data', (data) => {
+  console.log(data.toString());
+});
+
+bat.stderr.on('data', (data) => {
+  console.error(data.toString());
+});
+
+bat.on('exit', (code) => {
+  console.log(`Child exited with code ${code}`);
+});
