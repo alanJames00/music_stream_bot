58479315802236bbf601c2fcb51776a4f172b2da
alanjames00 <alanjames6282@gmail.com>
2023-10-28 21:35:54 +0530

    add indexjs params

--- index.js
+++ index.js
@@ -5,16 +5,21 @@
 // On Windows Only...
 import { spawn } from 'node:child_process';
 
-const ls = spawn('ls', ['-lh', '/usr']);
 
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
+const url = 'https://www.youtube.com/watch\?v\=bx3Vy0OmE8k';
+console.log(url);
 
-ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
 
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+// const ls = spawn('npx', ['youtube-exec', 'audio', '--url', ``]);
+
+// ls.stdout.on('data', (data) => {
+//   console.log(`stdout: ${data}`);
+// });
+
+// ls.stderr.on('data', (data) => {
+//   console.error(`stderr: ${data}`);
+// });
+
+// ls.on('close', (code) => {
+//   console.log(`child process exited with code ${code}`);
+// });
